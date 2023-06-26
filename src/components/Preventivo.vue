03d09208c727fbdcd1456a69b4c88233450b1188
Dino-996 <davidesabia22@gmail.com>
2023-06-26 02:44:19 +0200

    Modifiche minori in src/components/Preventivo.vue

--- src/components/Preventivo.vue
+++ src/components/Preventivo.vue
@@ -138,8 +138,8 @@ export default {
     </v-container>
 
     <v-container>
-        <v-form fast-fail methods="POST" name="contact" netlify on-submit="submit">
-            <input type="hidden" name="contact" value="New lead from %{formName} (%{submissionId})" autocomplete="on">
+        <v-form fast-fail methods="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value="contact">
             <v-text-field :rules="firstNameRules" density="compact" prepend-inner-icon="mdi-account" label="Nome o Azienda"
                 variant="underlined" color="light-blue" counter clearable aria-required="true" type="text" name="name"
                 autocomplete="on" />
