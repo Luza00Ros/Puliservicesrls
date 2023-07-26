d63c1d368d9108aba4953689ae5d63f2eea176ee
Luza00Ros <lucianarosati6@gmail.com>
2023-07-26 13:28:59 +0200

    Configurazione Firebase

--- src/main.js
+++ src/main.js
@@ -3,21 +3,26 @@ import App from './App.vue'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7sOv5hN1bj2B25lkoEowRMfZREVMTNwQ",
-  authDomain: "impresa-di-pulizie.firebaseapp.com",
-  projectId: "impresa-di-pulizie",
-  storageBucket: "impresa-di-pulizie.appspot.com",
-  messagingSenderId: "487930114231",
-  appId: "1:487930114231:web:84dc5f47db77fb5e835d32"
+  apiKey: "AIzaSyDDwDSok1-KwoikpE9ae1ERX72uis0CwCA",
+  authDomain: "puliservicesrls-4246e.firebaseapp.com",
+  projectId: "puliservicesrls-4246e",
+  storageBucket: "puliservicesrls-4246e.appspot.com",
+  messagingSenderId: "526752361518",
+  appId: "1:526752361518:web:dbe822ea617dd7827303bf",
+  measurementId: "G-6LT023J95R"
 };
 
 const FIREBASE_CAPTCHA_CODE = '6LfMSgMnAAAAAEygEVsRmIcS9FMMbAszuuosic_I';
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
+
+// Inizialize Analytics
+const analytics = getAnalytics(firebase);
 
 const appCheck = initializeAppCheck(firebase, {
   provider: new ReCaptchaV3Provider(FIREBASE_CAPTCHA_CODE),
@@ -34,4 +39,4 @@ const app = createApp(App)
 
 registerPlugins(app)
 
-app.use(firebase).use(appCheck).mount('#app')
+app.use(firebase).use(analytics).use(appCheck).mount('#app')
